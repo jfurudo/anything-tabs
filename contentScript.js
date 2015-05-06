de4b71045deb6fcd36cf7b255d9163afafad78c5
古堂　淳也　 <junya_furudo@jfurudo.local>
2015-05-06 18:04:04 +0900

    Added active class to the first anything row.

--- contentScript.js
+++ contentScript.js
@@ -1,54 +1,76 @@
-debugger;
-var $anythingDialog = null,
-    isOpenDialog = false;
-console.log("hello, content script js");
+// debugger;
+(function () {
+  var $anythingDialog = null,
+      previousText = "";
 
-document.onkeydown = function(e){
-  // Meta + b
-  if (e.keyCode === 66 && e.metaKey) {
-    if (!$anythingDialog) {
-      chrome.runtime.sendMessage("anything.tabs.getDialog", function (data) {
-        $anythingDialog = $(data)
-        console.log("return value: ", $anythingDialog);
-        $("body").append($anythingDialog);
-        $("#pattern").keydown(function (e) {
-          console.log("hoge", $(this).val());
-        });
-        // TODO: 2度目以降は使いまわす
-        $anythingDialog.on("hidden.bs.modal", function () {
-          $(this).remove();
-        })
-        $anythingDialog.on("shown.bs.modal", function () {
-          $("#pattern").focus();
-        });
-        $anythingDialog.modal("show");
+  var initializeDialog = function () {
+    chrome.runtime.sendMessage("anything.tabs.getDialog", function (data) {
+      $anythingDialog = $(data)
+      console.log("return value: ", $anythingDialog);
+      $("body").append($anythingDialog);
+      $("#pattern").keyup(function (e) {
+        if (previousText !== $(this).val()) {
+          var filterText = $(this).val();
+          previousText = filterText;
+          filterRow(filterText);
+          setActive(0);
+        }
       });
-    } else {
-      $anythingDialog.modal("toggle")
-    }
-    chrome.runtime.sendMessage("anything.tabs.getPageList", function (data) {
-      _.each(data, function (page) {
-        $("#anything-page-list").append(generateAnythingTabListColumn(page));
-      })
+      $anythingDialog.on("hidden.bs.modal", function () {
+        $(".anything-row").remove();
+      });
+      $anythingDialog.on("shown.bs.modal", function () {
+        $("#pattern").focus();
+      });
     });
-  }
-};
+  };
 
-// React とか使った方がいい？
-var generateAnythingTabListColumn = function (page) {
-  var col = $("<div class='row anything-col'></div>"),
-      imgContainer = $("<div class='col-md-1' />"),
-      favicon = $("<img class='anything-col-favicon' />"),
-      textContainer = $("<div class='col-md-11 anything-text-container' />"),
-      title = $("<h6 class='anything-col-title' />"),
-      url = $("<h6 class='anything-col-url'><small></small></h4>");
+  initializeDialog();
+  
+  var setActive = function (index) {
+    $(".anything-row.active").removeClass("active");
+    $(".anything-row.anything-shown:eq(" + index + ")").addClass("active");
+  };
+  
+  var filterRow = function (text) {
+    var regexp = new RegExp(text, "i");
+    $(".anything-row").filter(function (index) {
+      return !$(this).text().match(regexp);
+    }).hide().removeClass("anything-shown");
+    $(".anything-row").filter(function (index) {
+      return !!$(this).text().match(regexp);
+    }).show().addClass("anything-shown");;
+  };
 
-  favicon.attr({src: page.favIconUrl});
-  title.text(page.title);
-  url.children().text(page.url);
+  // React とか使った方がいい？
+  var generateRow = function (page) {
+    var row = $("<div class='row anything-row'></div>"),
+        imgContainer = $("<div class='col-md-1' />"),
+        favicon = $("<img class='anything-row-favicon' />"),
+        textContainer = $("<div class='col-md-11 anything-text-container' />"),
+        title = $("<h6 class='anything-row-title' />"),
+        url = $("<h6 class='anything-row-url'><small></small></h4>");
 
-  return col.append([
-    imgContainer.append(favicon),
-    textContainer.append([title, url])
-  ]);
-}
+    favicon.attr({src: page.favIconUrl});
+    title.text(page.title);
+    url.children().text(page.url);
+
+    return row.append([
+      imgContainer.append(favicon),
+      textContainer.append([title, url])
+    ]);
+  };
+
+  document.onkeydown = function(e){
+    // Meta + b
+    if (e.keyCode === 66 && e.metaKey) {
+      $anythingDialog.modal("toggle")
+      chrome.runtime.sendMessage("anything.tabs.getPageList", function (data) {
+        _.each(data, function (page) {
+          $("#anything-page-list").append(generateRow(page));
+        });
+      });
+    }
+  };
+
+})();
